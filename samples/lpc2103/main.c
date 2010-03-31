d96ea3514ae1659c112e8c49886ea77014fb958c
Ivan A-R <ivan@tuxotronic.org>
2010-04-01 03:46:40 +0400

    avr demo done

--- samples/lpc2103/main.c
+++ samples/lpc2103/main.c
@@ -46,6 +46,46 @@ int main(void)
     lcdSetBgColor(lcdGetColor(0, 255, 255));
     lcdClean();
     
+    const int color_table[] = {
+        lcdGetColor(0, 0, 0),
+        lcdGetColor(0, 0, 255),
+        lcdGetColor(0, 255, 0),
+        lcdGetColor(0, 255, 255),
+        lcdGetColor(255, 0, 0),
+        lcdGetColor(255, 0, 255),
+        lcdGetColor(255, 255, 0),
+        lcdGetColor(255, 255, 255),
+    };
+
+    for(;;)
+    {
+        int i;
+        for(i=0; i<8; i++)
+        {
+            int color = color_table[i];
+            int j;
+            for(j=0; j<4; j++)
+            {
+                int x, y;
+                int clr;
+                switch(j) {
+                    case 0: clr = color; x = 0; y = 0; break;
+                    case 1: clr = 7*color/8; x = 16; y = 0; break;
+                    case 2: clr = 6*color/8; x = 0; y = 16; break;
+                    case 3: clr = 5*color/8; x = 16; y = 16; break;
+                }
+                int xx, yy;
+                lcdSetFgColor(clr);
+                for(yy=y; yy<320; yy+=32)
+                    for(xx=x; xx<240; xx+=32)
+                        lcdBox(xx, yy, xx+15, yy+15);
+
+            }
+            delayMs(50);
+        }
+    }
+
+    /*
     lcdSetFgColor(lcdGetColor(255, 0, 0));
     lcdBox(0, 0, 50, 100);
     lcdSetFgColor(lcdGetColor(0, 255, 0));
@@ -56,10 +96,7 @@ int main(void)
     lcdSetFgColor(lcdGetColor(255, 0, 255));
     lcdSetBgColor(lcdGetColor(0, 255, 0));
     lcdFillRect(100, 100, 200, 200, 3);
-
-    while(1)
-    {
-    }
+    */
 
 	return 0;	
 }
